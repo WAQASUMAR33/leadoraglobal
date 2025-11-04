7445db6c9a70fb64a3b0a55f1888a54759ec7da2
Leadora Admin <admin@leadora.com>
2025-11-04 11:24:15 +0500

    Fix middleware matcher to exclude api routes and ensure favicon is served from public

--- src/middleware.js
+++ src/middleware.js
@@ -221,8 +221,9 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - public folder
+     * - api routes (handled separately)
      */
-    '/((?!_next/static|_next/image|favicon.ico|public/).*)',
+    '/((?!_next/static|_next/image|favicon.ico|public/|api/).*)',
   ],
 };
 
